1f2547324ca76d87e0fbc5932e7156073503f6d7
davidemarcoli <davide@marcoli.ch>
2023-06-27 09:58:32 +0200

    Add correct topics

--- environment-server/src/main/java/dev/dalame/Main.java
+++ environment-server/src/main/java/dev/dalame/Main.java
@@ -1,17 +1,20 @@
 package dev.dalame;
 
+import com.influxdb.client.DeleteApi;
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.InfluxDBClientFactory;
 import com.influxdb.client.WriteApiBlocking;
+import com.influxdb.client.domain.DeletePredicateRequest;
 import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.client.write.Point;
+import okhttp3.OkHttpClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.Instant;
-import java.util.Date;
+import java.time.OffsetDateTime;
 import java.util.Properties;
 import java.util.function.BiConsumer;
 import java.util.logging.Level;
@@ -20,7 +23,7 @@ import java.util.logging.Logger;
 public class Main {
     public static double temperature = 0;
     public static double humidity = 0;
-    private static Logger logger;
+    private static Logger logger = Logger.getLogger(Main.class.getName());
     private static Properties config;
     private static InfluxDBClient client;
     private static String influxdbBucket;
@@ -37,10 +40,16 @@ public class Main {
         return false;
     }
 
-    public static void main(String[] args) throws InterruptedException, MqttException {
+    public static void main(String[] args) throws InterruptedException {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.hostnameVerifier((hostname, session) -> true);
+
         if (!loadConfig()) return;
 
         logger.info("Config file loaded");
+
+//        InfluxDBClientOptions options = InfluxDBClientOptions.builder().url(config.getProperty("influxdb-url")).authenticateToken(config.getProperty("influxdb-token").toCharArray()).build();
+//        client = InfluxDBClientFactory.create(options);
 
         client = InfluxDBClientFactory.create(config.getProperty("influxdb-url"),
                 config.getProperty("influxdb-token").toCharArray());
@@ -48,12 +57,10 @@ public class Main {
         influxdbBucket = config.getProperty("influxdb-bucket");
         influxdbOrg = config.getProperty("influxdb-org");
 
-        Mqtt mqttClient = new Mqtt("tcp://cloud.tbz.ch:1883", "dalama");
+        Mqtt mqttClient = new Mqtt("tcp://cloud.tbz.ch:1883", "dalama-server");
         try {
             mqttClient.start();
-            mqttClient.subscribe("Dalama/Subscribe");
-            mqttClient.subscribe("Dalama/Temp");
-            mqttClient.publish("Dalama/Publish", new Date().toString());
+            mqttClient.subscribe("Dalama/#");
         } catch (MqttException e) {
             e.printStackTrace();
         }
@@ -63,10 +70,10 @@ public class Main {
         mqttClient.addHandler(new BiConsumer<String, MqttMessage>() {
             @Override
             public void accept(String s, MqttMessage mqttMessage) {
-                if (s.equals("m5core2/temp")) {
+                if (s.startsWith("Dalama/temperature/")) {
                     temperature = Double.parseDouble(mqttMessage.toString());
                 }
-                if (s.equals("m5core2/hum")) {
+                if (s.startsWith("Dalama/humidity/")) {
                     humidity = Double.parseDouble(mqttMessage.toString());
                 }
             }
@@ -75,7 +82,8 @@ public class Main {
         double lastTemperature = temperature;
 
         while (true) {
-            if (Math.abs(lastTemperature - temperature) >= 1) {
+            // TODO: change 0.01 to a more realistic value
+            if (Math.abs(lastTemperature - temperature) >= 0.01) {
                 System.out.printf("Temperature changed. Current: %.2f%n", temperature);
                 Point point = Point
                         .measurement("temperature")
